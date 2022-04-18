51c44ba3b6dc71b06c68886567164c760d4c2570
Dmitry Sholomitski <xaos613@gmail.com>
2022-04-18 16:07:12 +0300

    Tasks 1-3

---
+++ Dmitry Sholomitski/2.1.py
@@ -0,0 +1,14 @@
+def str_len(string):
+    '''
+
+    :param string: input string
+    :return: lenth
+    '''
+    counter = 0
+    for letter in string:
+        counter += 1
+    return counter
+
+
+
+print(str_len(input('Enter the string: ')))
