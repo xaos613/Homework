bee858b17b17561dd055bdb8d1ee2e76e0d16a12
Dmitry Sholomitski <xaos613@gmail.com>
2022-06-06 11:29:20 +0300

    fixed 7.6

--- Dmitry Sholomitski/7.6.py
+++ Dmitry Sholomitski/7.6.py
@@ -24,10 +24,6 @@ class EmptyException(CustomException):
     pass
 
 
-class BoolException(CustomException):
-    pass
-
-
 class ValueException(CustomException):
     pass
 
@@ -51,12 +47,19 @@ def get_primes(num: int) -> list:
     return primes
 
 
-def chckerfunc(number=None):
+def chckerfunc(number):
     if number is None:
         raise EmptyException('There should  be at least 1 argument')
-    elif not isinstance(number, int):
-        raise BoolException(('There should be integer argument'))
-    return number % 2 == 0
+    else:
+        try:
+            number = int(number)
+        except (ValueError, TypeError):
+            raise TypeException(('There should be integer argument'))
+
+    if number < 4:
+        raise ValueException(f"Input integer should be more than 3, you gave {number}")
+    elif number % 2 == 0:
+        return number
 
 
 def get_input():
@@ -65,25 +68,21 @@ def get_input():
 
 
 def goldbach(number):
-    if chckerfunc(number):
-        if number < 4:
-            raise ValueException("Input integer should be more than 3")
-        else:
-            primes = get_primes(number)
-            goldbach_result = [[number - x, x] for x in primes if number - x in primes]
-            return (number, goldbach_result[-1])
+    number = chckerfunc(number)
+    primes = get_primes(number)
+    goldbach_result = [[number - x, x] for x in primes if number - x in primes]
+    return (number, goldbach_result[-1])
 
 
 def main():
-
     while True:
         choise = get_input()
         if choise in ['q', 'Q']:
             print("Thank you for using my script. Bye")
             break
         else:
+            num = choise if choise else None
             try:
-                num = int(choise)
                 print(goldbach(num))
             except ValueException as message:
                 print(f'Incorrect input: {message}')
@@ -91,8 +90,7 @@ def main():
                 print(f'Incorrect input: {message}')
             except TypeException as message:
                 print(f'Incorrect input: {message}')
-            except Exception as message:
-                print(f'Something got wrong: {message}')
+
 
 if __name__ == '__main__':
     main()
